90095c6d69e68135044d023070b3f4b1726e54c6
Kota Kanazawa <kota0818@gmail.com>
2024-09-03 19:52:31 +0900

    input files shows the query when refreshing the page after a search

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 }
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>()
+  const { contacts, q } = useLoaderData<typeof loader>()
   const navigation = useNavigation();
 
   return (
@@ -49,6 +49,7 @@ export default function App() {
               <input
                 id="q"
                 aria-label="Search contacts"
+                defaultValue={q || ""}
                 placeholder="Search"
                 type="search"
                 name="q"
