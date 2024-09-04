32fe3949030d32b8cc85eb621389ad3dfe7900a4
Kota Kanazawa <kota0818@gmail.com>
2024-09-04 20:25:24 +0900

    Optimistic UI

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -89,8 +89,8 @@ export default function Contact() {
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
-  const favorite = contact.favorite;
   const fetcher = useFetcher();
+  const favorite = fetcher.formData ? fetcher.formData.get("favorite") === "true" : contact.favorite;
 
   return (
     <fetcher.Form method="post">
